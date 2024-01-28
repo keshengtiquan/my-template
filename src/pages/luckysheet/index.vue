47e904731b72a6125e55674081eacc2d834d0f94
dadaz <keshengtiquan@163.com>
2024-01-28 09:11:13 +0800

    onlyoffice

--- src/pages/luckysheet/index.vue
+++ src/pages/luckysheet/index.vue
@@ -1,23 +1,19 @@
 <script lang="ts" setup>
 import { DocumentEditor } from "@onlyoffice/document-editor-vue";
+import {onMounted, ref} from "vue";
+import {getDocunmentInfoApi} from "@/api/onlyoffice";
 
-const config = {
-  document: {
-    fileType: "docx",
-    key: "test.docx",
-    title: "Example Document Title.docx",
-    url: "http://192.168.31.115:3000/static/test1.docx"
-  },
-  documentType: "word",
-  editorConfig: {
-    callbackUrl: "https://example.com/url-to-callback.ashx"
-  }
-}
+const config = ref()
+onMounted(async () => {
+  const {data} = await getDocunmentInfoApi({key: '变电所（东南段）.xlsx', fileType: 'xlsx', documentType: 'cell'})
+  config.value = data
+})
 </script>
 
 <template>
-  <DocumentEditor id="docEditor" documentServerUrl="http://keshengtiquan.work:9500/" :config="config"
-     />
+    <DocumentEditor  id="docEditor" documentServerUrl="http://keshengtiquan.work:9500/" :config="config"
+    />
+
 </template>
 
 <style scoped></style>
